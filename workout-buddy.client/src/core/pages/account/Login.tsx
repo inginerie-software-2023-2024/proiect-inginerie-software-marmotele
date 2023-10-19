37e087d1c6e51c25331eeadf43149fb554109a6a
Sergiu Stanciu <sergiu.stanciu@essensys.ro>
2023-10-19 14:31:13 +0300

    creating login page and migrating to chakra ui styles

---
+++ workout-buddy.client/src/core/pages/account/Login.tsx
@@ -0,0 +1,115 @@
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { accountActions } from "../../../store/reducers/account";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+
+const loginModelInitialState = {
+  email: "",
+  password: "",
+  areCredentialsInvalid: false,
+  isDisabled: false,
+};
+
+export default function Login() {
+  const dispatcher = useDispatch();
+
+  const [loginModel, setLoginModel] = useState(loginModelInitialState);
+
+  const submitHandler = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    const res = await axios({
+      method: "post",
+      url: "https://localhost:7132/UserAccount/login",
+      data: loginModel,
+    });
+    dispatcher(accountActions.login(res.data));
+
+    window.location.href = "/";
+  };
+
+  return (
+    <Flex
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("white", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("gray.50", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+          borderColor="lightPallette.primary.main"
+          borderWidth="2px"
+        >
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input
+                type="email"
+                value={loginModel.email}
+                onChange={(e) =>
+                  setLoginModel({ ...loginModel, email: e.target.value })
+                }
+              />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input
+                type="password"
+                value={loginModel.password}
+                onChange={(e) =>
+                  setLoginModel({ ...loginModel, password: e.target.value })
+                }
+              />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              >
+                <Checkbox>Remember me</Checkbox>
+                <Link
+                  color={useColorModeValue(
+                    "lightPallette.accent.main",
+                    "darkPallette.accent.main"
+                  )}
+                >
+                  Forgot password?
+                </Link>
+              </Stack>
+              <Button
+                colorScheme={useColorModeValue(
+                  "lightPallette.primary",
+                  "darkPallette.primary"
+                )}
+                onClick={submitHandler}
+              >
+                Sign in
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
