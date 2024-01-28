b2481f0212e75fee61318355779e698150ffe054
Alexia Aldea <alexia.elena.aldea@gmail.com>
2024-01-28 13:39:26 +0200

    fixes

--- WorkoutBuddy.Mobile/WorkoutBuddyMobileiOS/WorkoutBuddyMobileiOS/Utils/CustomColors.swift
+++ WorkoutBuddy.Mobile/WorkoutBuddyMobileiOS/WorkoutBuddyMobileiOS/Utils/CustomColors.swift
@@ -13,7 +13,6 @@ struct CustomColors {
     static let background = Color("background")
     static let myBlue = Color("blue")
     static let myDarkGray = Color("dark_gray")
-    static let myError = Color("error")
     static let backgroundDark = Color("background_dark")
     static let buttonDark = Color("button_dark")
 }
